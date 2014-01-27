8fe61fcaa8d716abd503e326efe346c18dc29445
Eiichiro Uchiumi <eiichiro.uchiumi@gmail.com>
2014-01-28 01:10:24 +0900

    Some bug fixes

--- gig-archetype-heroku/src/main/resources/archetype-resources/src/main/java/WelcomeEndpoint.java
+++ gig-archetype-heroku/src/main/resources/archetype-resources/src/main/java/WelcomeEndpoint.java
@@ -4,6 +4,7 @@ import static org.eiichiro.bootleg.MediaType.*;
 import static org.eiichiro.bootleg.Verb.*;
 
 import org.eiichiro.jaguar.inject.Inject;
+import org.eiichiro.jaguar.inject.Name;
 import org.eiichiro.bootleg.annotation.Allows;
 import org.eiichiro.bootleg.annotation.Endpoint;
 import org.eiichiro.bootleg.annotation.Generates;
